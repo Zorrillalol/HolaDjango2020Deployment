8cae9d72d7050d0e2073a5a02fe3b8e148442139
zorrillalol <luis56587@gmail.com>
2020-02-12 09:29:22 -0600

    Agrega prueba de smoke y archivo de configuración travis

--- hola/tests.py
+++ hola/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestSmoke(TestCase):
+	"""docstring for TestSmoke"""
+	def test_smoke_Test(self):
+		self.assertEquals(2+2,4)
+		
